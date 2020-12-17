53af4de6e4dc77120bb0e82c1da035e0e7e3bbec
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-17 13:59:04 +0100

    Avvio progetto

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Attraverso una chiamata ajax all’API di boolean
+// https://flynn.boolean.careers/exercises/api/array/music
+// avremo a disposizione una decina di dischi musicali.
+// Utilizzando vue, stampiamo a schermo una card per ogni album.
