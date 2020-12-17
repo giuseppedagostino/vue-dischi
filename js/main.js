5c038725ba21903a89174784b5c31d918f5c9cdc
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-17 15:31:10 +0100

    Chiamata al server

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-// Attraverso una chiamata ajax all’API di boolean
-// https://flynn.boolean.careers/exercises/api/array/music
-// avremo a disposizione una decina di dischi musicali.
+// Attraverso una chiamata ajax all’API di boolean avremo a disposizione una decina di dischi musicali.
 // Utilizzando vue, stampiamo a schermo una card per ogni album.
 
 var app = new Vue ({
@@ -9,6 +7,13 @@ var app = new Vue ({
 
   },
   methods: {
-    
+
+  },
+  mounted: function() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then( function(result) {
+        console.log(result.data.response);
+      })
   }
 });
