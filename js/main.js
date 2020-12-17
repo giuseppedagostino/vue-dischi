32ffbe37437ff4a1019525779c9260569eeea744
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-17 15:02:05 +0100

    Prima bozza css

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@
 // https://flynn.boolean.careers/exercises/api/array/music
 // avremo a disposizione una decina di dischi musicali.
 // Utilizzando vue, stampiamo a schermo una card per ogni album.
+
+var app = new Vue ({
+  el: "#root",
+  data: {
+
+  },
+  methods: {
+    
+  }
+});
